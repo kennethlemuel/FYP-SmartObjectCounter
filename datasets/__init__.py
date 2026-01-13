892202c410a967fcfd21db49e8cbc1a9b347893a
#KENNETH LEMUEL# <kenneth0@asp2a-login-ntu02.head.cm.asp2a.nscc.sg>
2026-01-14 02:06:47 +0800

    modified pbs files for better fairness against SOTA comparison and new init file

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,11 +1,9 @@
-from .rgbt_cc import RGBTCC_RGBDataset, load_points
-from .shtb import SHTBDataset
 from .density import density_from_points_knn, density_from_points_fixed
+from .shtb import SHTBDataset
+
 
 __all__ = [
-    "RGBTCC_RGBDataset",
-    "SHTBDataset",
-    "load_points",
     "density_from_points_knn",
     "density_from_points_fixed",
+    "SHTBDataset",
 ]
